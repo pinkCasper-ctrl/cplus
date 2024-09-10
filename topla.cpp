3aa3e719be1e3687c7375fce1e3499ddd3d8086b
pinkCasper-ctrl <bulut173.hatice@gmail.com>
2024-09-10 17:04:57 +0300

    Update topla.cpp

--- topla.cpp
+++ topla.cpp
@@ -9,7 +9,9 @@ using namespace std;
 int main()
 { 
 	setlocale(LC_ALL,"Turkish"); //Türkçe karakter
-	int sayi1,sayi2,toplam;
+	int sayi1;
+	int sayi2;
+	int toplam;
 	cout<<"1. Sayı : ";
 	cin>>sayi1;
 	cout<<"2. Sayı : ";
