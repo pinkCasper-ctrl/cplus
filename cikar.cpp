a8a95b6173d27082fff9a6be1a9e9e6e4d0ca5d8
pinkCasper-ctrl <bulut173.hatice@gmail.com>
2024-09-10 14:57:02 +0300

    Create cikar.cpp

---
+++ cikar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<stdlib.h>
+#include <cmath>
+using namespace std;
+ 
+ 
+ 
+int main()
+{ 
+	setlocale(LC_ALL,"Turkish"); //Türkçe karakter
+	int sayi1,sayi2,toplam;
+	cout<<"1. Sayı : ";
+	cin>>sayi1;
+	cout<<"2. Sayı : ";
+	cin>>sayi2;
+	tocikarma=sayi2-sayi1;
+	cout<<"Çıkarma : "<<cikarma;
+}
